c61dcdbf2d0080f203579af5b264af64f23b3cdb
Keenan Jones <keenanjones210@gmail.com>
2019-12-25 23:32:01 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,7 +45,7 @@ def naming_system
   :sub_category => {:label => "Phylum", 
   :sub_category => {:label => "Order", 
   :sub_category => {:label => "Class", 
-  :sub_category => {:label => "Family",
-  :sub_category => {:label => "Genus", 
+  :sub_category => {:label => "Genus",
+  :sub_category => {:label => "Family", 
   :label => "Species"}}}}}}}
 end
